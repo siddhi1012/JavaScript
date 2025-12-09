f19692c176f721491840a276a9c95859f063eed7
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-09 17:10:47 +0530

    Updated files

--- Practice_Part_8.js
+++ Practice_Part_8.js
@@ -1,5 +1,5 @@
 // forEach
-
+let arr = [1,2,3,4,5,6,7,8];
 arr.forEach(function(el){
     console.log(el);
 });
@@ -11,3 +11,22 @@ let  double = arr.map(function (el){
 });
 
 // Student Object 
+let student = [
+    {
+    name : "prasad",
+    marks : 95
+    },
+    {
+    name : "siddhi",
+    marks : 92    
+    },
+    {
+    name : "Rukku",
+    marks : 97
+    }
+];
+
+let gpa = student.map(function(el){
+    return el.marks / 10;
+
+});
