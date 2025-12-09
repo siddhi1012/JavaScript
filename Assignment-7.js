d46f2525069ef90576c50c50580d66b2ce4204be
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-09 14:51:03 +0530

    Updated files

---
+++ Assignment-7.js
@@ -0,0 +1,21 @@
+// Question - 1 
+let arr = [1,2,3,4,5,6];
+const arrayAverage = (arr) =>{
+    let total = 0;
+    for(let number of arr){
+        total = total + number;
+    }
+    return total / arr.length;
+}
+console.log(arrayAverage(arr));
+
+// Question - 2 
+
+let num = 4;
+const isEven = (num) => {
+    num % 2 == 0 ;
+    return  num +" "+"is Even Number";
+
+}
+
+console.log(isEven(num));
