5a3db47cea0dbd0ccd397df0555c0d02c6ea251f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-09 17:22:18 +0530

    Updated files

--- Practice_Part_8.js
+++ Practice_Part_8.js
@@ -30,3 +30,9 @@ let gpa = student.map(function(el){
     return el.marks / 10;
 
 });
+
+// filter 
+let nums = [2,4,1,5,6,2,7,8,9];
+let even = nums.filter((nums) => { 
+    return nums % 2 == 0
+});
