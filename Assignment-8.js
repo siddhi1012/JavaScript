5d182dd7e5bef4ca23dece096fe8165bc585d73f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-10 09:30:36 +0530

    Updated files

--- Assignment-8.js
+++ Assignment-8.js
@@ -12,3 +12,34 @@ console.log(avg)
 
 //Question-2
 console.log("Question-2");
+let numbers = [2,4,8,-2,-4];
+let res = numbers.map((numbers) => numbers + 5);
+console.log(res);
+
+//Question-3
+console.log("Question-3");
+let string = ["prasad","siddhi","sakshi","aryan","rukku"];
+let result = string.map((string) => string.toUpperCase());
+console.log(result);
+
+//Question-4
+console.log("Question-4");
+const doubleAndReturnArgs= (arr,...args) =>[
+    ...arr,
+    ...args.map((v) => v*2),
+];
+doubleAndReturnArgs([1,2,3],4,4);
+doubleAndReturnArgs([2],10,4);
+
+//Question-5
+console.log("Question-5");
+const mergeObject = (obj1,obj2) =>({...obj1,...obj2});
+mergeObject(
+    {
+        a : 1,
+        b : 2
+    },
+    {
+        c : 3,
+        d : 4
+    });
