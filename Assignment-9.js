c9d0759f991cdda8112e8913e139d5cec7ae4f8f
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-10 19:16:57 +0530

    Updated files

---
+++ Assignment-9.js
@@ -0,0 +1,7 @@
+// QS-1
+let btn = document.createElement('button');
+let input = document.createElement('input');
+let body = document.querySelector('body');
+btn.innerText = "Click Me!!!";
+body.append(input);
+body.append(btn);
