751726b72322f78928bdb73ad9a4f64af1d90316
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-11 18:53:01 +0530

    Updated files

--- formEvent.js
+++ formEvent.js
@@ -1,7 +1,25 @@
 let form = document.querySelector("form");
 form.addEventListener("submit", function (){
     event.preventDefault();
-    let inp = document.querySelector("input");
-    console.dir(inp);
-    console.log(inp.value);
+    let user = this.elements[0];
+    let pass = this.elements[1];
+    // let user = document.querySelector("#user");
+    // let pass = document.querySelector("#pass");
+    console.log(user.value);
+    console.log(pass.value);
+    alert(`Hi ${user.value}, Your Password is set to ${pass.value}`);
 })
+let user = document.querySelector("#user");
+user.addEventListener("change", function (){
+    event.preventDefault();
+    console.log("Input Value");
+    console.log("final Value",this.value);
+
+});
+
+user.addEventListener("input", function (){
+    event.preventDefault();
+    console.log("Input Changed");
+    console.log("final Value",this.value);
+
+});
