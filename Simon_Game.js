e2263cc84107976e78186bf3492802613b11f2bc
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-12 22:02:23 +0530

    Updated files

--- Simon_Game.js
+++ Simon_Game.js
@@ -51,6 +51,10 @@ function checkAns(idx){
         }
     }else{
         h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout ( function (){
+             document.querySelector("body").style.backgroundColor = "white";
+        },150);
         reset();
     }
 }
