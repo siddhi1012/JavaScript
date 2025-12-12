840010194e7ef7037cf03628a2a0e9b463669db8
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-13 00:01:40 +0530

    Updated files

--- part_11.js
+++ part_11.js
@@ -1,3 +1,4 @@
+// Practice - 1
 function sayHello(){
     console.log("Hello World");
 }
@@ -5,5 +6,16 @@ function sayHello(){
 function Demo(){
     sayHello();
 }
-
 Demo();
+
+// Practice - 2
+function one(){
+    return 1;
+}
+function two(){
+    return one() + one();
+}
+function three(){
+    let ans = two() + one();
+    console.log(ans);
+}
