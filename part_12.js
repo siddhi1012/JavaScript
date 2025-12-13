a2aeb782481b5321e6b46c697da66a87cd64f7ba
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-13 21:50:34 +0530

    Updated files

--- part_12.js
+++ part_12.js
@@ -1,3 +1,17 @@
 async function greet () {
+    throw "weak Connection";
     return "Hello World";
+}
+greet()
+.then((result) => {
+    console.log("Promises was resolved");
+    console.log("result was:",result);
+})
+.catch((err) =>{
+    console.log("Promises was rejected");
+    console.log("Error was:",err);   
+});
+
+let demo = async() =>{
+    return 5 ;
 }
