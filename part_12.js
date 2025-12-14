52a2f7357a944c72e285c9018378a48d68c20eed
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-14 10:44:50 +0530

    Updated files

--- part_12.js
+++ part_12.js
@@ -56,4 +56,39 @@ async function Demo2(){
 }
 
 let url = "https://catfact.ninja/fact";
-fetch(url);
+// fetch(url)
+// .then((res) =>{
+//     console.log(res);
+//     // console.log(res.json()); // read Data
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log("Data 1:",data.fact);
+//     return fetch(url);
+// })
+// .then((res) => {
+//     return res.json();
+
+// })
+// .then((data2) => {
+//     console.log("Data 2:",data2.fact);
+
+// })
+// .catch((err) => {
+//     console.log("Error :",err);
+// });
+
+async function getFacts(){
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);        
+    }catch(e){
+        console.log("Error",e);
+    }
+
+}
